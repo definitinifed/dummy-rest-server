1335529dc4a35f06200df84124759406d3b2fb0c
Fikret Buyukisik <fikret.buyukisik2@bam.com>
2020-03-02 13:10:23 +0100

    correct port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const audit = require('express-request-logger')
 const bodyParser = require('body-parser')
-const port = 3000;
+const port = process.env.PORT || 80;
 
 const app = express()
 
