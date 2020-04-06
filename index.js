71b4d883b08497a2e7c4df6da43ba41a7afe9beb
Fikret Buyukisik <fikret.buyukisik2@bam.com>
2020-04-06 11:22:49 +0200

    cors issue

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 const express = require('express')
 const audit = require('express-request-logger')
 const bodyParser = require('body-parser')
+const cors = require('cors');
 const swagger = require('./swagger-integration');
 const appointment = require('./api/appointment');
 
 const port = process.env.PORT || 80;
 
 const app = express()
-
+app.use(cors());
 // parse application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({ extended: false }))
 
